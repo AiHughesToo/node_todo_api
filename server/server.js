5daf787e3ca5e35babacc70e1df4a65b6104d84d
Alex Hughes <alex.hughes@interapthq.com>
2018-03-12 16:07:27 -0400

    setup first endpoint

--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
-var mongoose = require('mongoose');
 
-// connect to the database
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+//library imports
+var express = require('express');
+var bodyParser = require('body-parser');
+//local imports
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String
-  },
-  completed: {
-   type: Boolean
-  },
-  completedAt: {
-   type: Number
-  }
+// Set up the app
+var app = express();
+
+//middleware
+app.use(bodyParser.json());
+
+// this works like a combo of routes file and the controller in Rails
+app.post('/todos', (req, res) => {
+  // make a new isntance of a todo
+  var todo = new Todo ({
+    text: req.body.text
+  });
+  // save the the record and respond to the user.
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+//starting the server
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
