7beef261471a14f9d37e07b0e93bd9bf6580c817
Alex Hughes <alex.hughes@interapthq.com>
2018-03-13 14:35:31 -0400

    setup testing

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,9 +3,13 @@ const request = require('supertest');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+//seed file
+const todos = [{text: 'First test todo'},{text: 'second test todo'}];
 
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -19,7 +23,7 @@ describe('POST /todos', () => {
                   if (err) {
                     return done(err);
                   }
-                  Todo.find().then((todos) => {
+                  Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -27,16 +31,26 @@ describe('POST /todos', () => {
                 });
   });
 // setup the test and define what it should do.
-  it('should not create todo with invalid data', (done) =>{
+  it('should not create todo with invalid data', (done) => {
 // send the request to the endpoint
     request(app).post('/todos').send({}).expect(400).end((err, res) => {
       // handle the inital error and return so nothing else runs
       if (err) { return done(err); }
       // check the database if the record was created
       Todo.find().then((todos) => {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done();
       }).catch((e) => done(e));
     });
+  });
+});
+
+// new test for get request
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app).get('/todos').expect(200)
+    .expect((res) => {
+      expect(res.body.todos.length).toBe(2);
+    }).end(done);
   });
 });
